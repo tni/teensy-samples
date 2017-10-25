6d1e9ef51c6f226a0d203d1991810ec4a087b966
Thierry <thierry@theremin.tf>
2017-10-25 13:52:28 +0200

    Update dac_fifo_dma.ino
    Some extensions and explanations

--- dac_fifo_dma.ino
+++ dac_fifo_dma.ino
@@ -15,9 +15,9 @@
 // but rather a ringbuffer. There is an index to the currently used entry.
 //
 // DMA transfers can be triggered by the FIFO index being at 0 and
-// the FIFO watermark. This code uses these triggers to transfer 4 entries
-// at a time. The lower half of the FIFO (entries 0 - 3) is updated when
-// the FIFO index reaches 4 (the watermark), the upper half (entries 4 - 7)
+// the FIFO watermark. This code uses these triggers to transfer 8 entries
+// at a time. The lower half of the FIFO (entries 0 - 7) is updated when
+// the FIFO index reaches 11 (maximum - 4) (= the watermark), the upper half (entries 8 - 15)
 // is updated when the FIFO index wraps to 0.
 //
 // The FIFO DMA destination is set up using the DMA modulo feature,
@@ -29,6 +29,8 @@
 
 #include <array>
 #include <DMAChannel.h>
+#define TRACE_VIEW 1
+
 
 const size_t buffer_alignment = 8;
 std::array<uint16_t, 512> buffer __attribute__ ((aligned (buffer_alignment))) DMAMEM;
@@ -37,11 +39,8 @@ static_assert(buffer_byte_count % buffer_alignment == 0, "DMA buffer end not pro
 static_assert(buffer.size() % 8 == 0, "Buffer size must be multiple of DAC buffer.");
 
 void initBuffer() {
-    for(size_t i = 0; i < buffer.size(); i += 4) {
-        buffer[i+0] = 0;
-        buffer[i+1] = 1000;
-        buffer[i+2] = 2000;
-        buffer[i+3] = 3000;
+    for(size_t i = 0; i < buffer.size(); i ++) {
+        buffer[i] = (int16_t)(2047 * sinf(6.2831853 * i / buffer.size())) +2048;
     }
 }
 
@@ -58,36 +57,37 @@ void setup() {
     DAC0_C0 |= DAC_C0_DACRFS;     // use 3.3V VDDA as reference voltage
     DAC0_C0 |= DAC_C0_DACBWIEN;   // enable DMA trigger at watermark
     DAC0_C0 |= DAC_C0_DACBTIEN;   // enable DMA trigger at 0
-    DAC0_C1 = DAC_C1_DACBFWM(2) | // watermark for DMA trigger
-                                  // --> DMA triggered when DAC buffer index is 4
+    DAC0_C1 = DAC_C1_DACBFWM(3) | // watermark for DMA trigger
+                                  // --> DMA triggered when DAC buffer index is 4 words away from DACBFUP, that means at index 11 here
+                                  // which gives DMA still enough time to refill the ring buffer from index 0 to 7
               DAC_C1_DMAEN      | // enable DMA
               DAC_C1_DACBFEN;     // enable DAC buffer
 
-    DAC0_C2 = DAC_C2_DACBFUP(7);  // set buffer size to 8
+    DAC0_C2 = DAC_C2_DACBFUP(15);  // set buffer size to 16
 
     DAC0_SR &= ~(DAC_SR_DACBFWMF); // clear watermark flag
     DAC0_SR &= ~(DAC_SR_DACBFRTF); // clear top pos flag
     DAC0_SR &= ~(DAC_SR_DACBFRBF); // clear bottom pos flag
 
-    // Init DAC FIFO with the last 8 buffer elements. This makes setting up the circular
+    // Init DAC FIFO with the last 16 buffer elements. This makes setting up the circular
     // DMA transfer easier.
     using aliased_uint16 = uint16_t __attribute__((__may_alias__));
     using aliased_uint16_vptr = volatile aliased_uint16*;
-    for(size_t i = 0; i < 8; i++) {
-        ((aliased_uint16_vptr) &DAC0_DAT0L)[i] = buffer[buffer.size() - 8 + i];
+    for(size_t i = 0; i < 16; i++) {
+        ((aliased_uint16_vptr) &DAC0_DAT0L)[i] = buffer[buffer.size() - 16 + i];
     }
 
     // The modulo feature of the DMA controller is used. The destination
     // pointer wraps at +16 bytes.
-    dma.destinationCircular(((volatile uint16_t*) &DAC0_DAT0L), 16);
+    dma.destinationCircular(((volatile uint16_t*) &DAC0_DAT0L), 32);
 
     dma.TCD->SADDR = buffer.data();
     dma.TCD->SOFF = 2;
     dma.TCD->ATTR_SRC = 1;
-    dma.TCD->NBYTES = 8;
+    dma.TCD->NBYTES = 16;
     dma.TCD->SLAST = -buffer_byte_count;
-    dma.TCD->BITER = buffer_byte_count / 8;
-    dma.TCD->CITER = buffer_byte_count / 8;
+    dma.TCD->BITER = buffer_byte_count / 16;
+    dma.TCD->CITER = buffer_byte_count / 16;
 
     dma.triggerAtHardwareEvent(DMAMUX_SOURCE_DAC0);
     dma.enable();
@@ -108,9 +108,10 @@ void setup() {
     PDB0_MOD = mod;
 
     // The hardware doesn't do what the manual claims. The DAC trigger counter is
-    // not reset on PDB counter overflow. Things work correctly, if the PDB mod
-    // is used.
-    PDB0_DACINT0 = mod;                // trigger DAC once per PDB cycle
+    // not reset on PDB counter overflow. Things work correctly, if the PDB mod or
+    // ((PDBmod + 1) / n) -1 which subdivides the PDB cycle by n, are used.
+    // The missing reset on overflow allows even "uneven" divisions...
+    PDB0_DACINT0 = 0x1554;             // trigger DAC 12.00293 times per PDB cycle, just as an example
     PDB0_DACINTC0 = PDB_DACINTC_TOE;   // enable DAC interval trigger
 
     PDB0_SC |= PDB_SC_LDOK;            // sync buffered PDB registers
@@ -126,11 +127,14 @@ void setup() {
         unsigned dac_out_idx_curr = DAC0_C2 >> 4;
         unsigned dac_val = ((aliased_uint16_vptr) &DAC0_DAT0L)[dac_out_idx_curr];
         interrupts();
-        if(src_idx_prev == src_idx_curr && dac_out_idx_prev == dac_out_idx_curr) continue;
+        if(/*src_idx_prev == src_idx_curr &&*/ dac_out_idx_prev == dac_out_idx_curr) continue;
         src_idx_prev = src_idx_curr;
         dac_out_idx_prev = dac_out_idx_curr;
-        Serial.printf("DMA src idx: %4u   DMA dest idx: %4u   DAC out idx: %4u   DAC value: %4u\n",
+        if (!TRACE_VIEW)
+          Serial.printf("DMA src idx: %4u   DMA dest idx: %4u   DAC out idx: %4u   DAC value: %4u\n",
                       src_idx_curr, dest_idx, dac_out_idx_curr, dac_val);
+        else
+          Serial.println(dac_val); // This allows to see the waveform in the tracer
     }
 }
 
